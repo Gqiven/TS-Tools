b7d76083c11adf98a10e50b0d15117c42a0dd571
xuqinwen <xuqinwen-jk@360shuke.com>
2022-08-24 11:54:17 +0800

    add useBoolean

---
+++ vue3-hooks/useToggle/index.ts
@@ -0,0 +1,30 @@
+import { ref } from "vue"
+
+interface Actions<T> {
+  setLeft: () => void,
+  setRight: () => void,
+  toggle: () => void,
+  set: (v: T) => void
+}
+
+function useToggle<T, U>(defaultValue: T, reverseValue: U): [T | U, Actions<T | U>]
+
+function useToggle<D, R>(
+  defaultValue: D = false as unknown as D,
+  reverseValue?: R// 相对值
+) {
+  const state = ref<D | R>(defaultValue)
+
+  const reverseValueOrigin = (reverseValue === undefined ? !defaultValue : reverseValue) as D | R;
+
+  const actions = {
+    toggle: () => state.value = (state.value === defaultValue ? reverseValueOrigin : defaultValue) as any,
+    set: (value: D | R) => state.value = value as any,
+    setLeft: () => state.value = reverseValueOrigin as any,
+    setRight: () => state.value = (state.value === defaultValue ? reverseValueOrigin : defaultValue) as any
+  }
+
+  return [state, actions]
+}
+
+export default useToggle
--- vue3-hooks/utils/index.ts
+++ vue3-hooks/utils/index.ts
@@ -1,8 +1,7 @@
-export const isFunction = (value: unknown): value is Function => typeof value === 'function'
+export const isFunction = (value: unknown): value is Function => typeof value === 'function';
+export const isString = (value: unknown): value is string => typeof value === 'string';
+export const isUndefined = (value: unknown): value is undefined => typeof value === 'undefined';
 
-export const isString = (value: unknown): value is string => typeof value === 'string'
-
-export const isUndefined = (value: unknown): value is undefined => typeof value === 'undefined'
 
 export const isBrowser = !!(
   typeof window !== 'undefined' &&
